f8013f1ad043798d322b854579b8495953269e2d
Denis Dolia <spda777@gmail.com>
2023-03-31 12:49:54 +0300

    last version

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Home } from './pages/Home';
+import { About } from './pages/About';
+import { Navbar } from './components/Navbar';
+import { Alert } from './components/Alert';
+import { AlertState } from './context/alert/AlertState';
+import { FirebaseState } from './context/firebase/FirebaseState';
+
 function App() {
   return (
-    <div className="Container">
-        <h1>test</h1>
-     
-    </div>
+    <FirebaseState>
+      <AlertState>
+        <BrowserRouter>
+          <Navbar />
+          <div className="container pt-4">
+            <Alert />
+            <Routes>
+              <Route exact="true" path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+            </Routes>
+          </div>
+        </BrowserRouter>
+      </AlertState>
+    </FirebaseState>
   );
 }
 
